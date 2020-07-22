980de452b284e04d75d25c9eff21e1866e9ff2cb
chloekim917 <chloekim917@gmail.com>
2020-07-22 11:09:09 -0400

    delete button done

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
+import Transaction from "./Transaction";
 
 class AccountContainer extends Component {
   state={
@@ -20,13 +21,28 @@ class AccountContainer extends Component {
 
   handleSearch=(e)=>this.setState({search:e.target.value})
 
+  deleteTransaction=(TransactionId)=>{
+    fetch(`http://localhost:6001/transactions/${TransactionId}`,{
+      method: 'DELETE',
+      headers: {
+        "content-type": 'application/json',
+        accept: 'application/json'
+      }
+    })
+    .then(resp=>resp.json())
+    .then(()=>{
+      let revisedTransactionList= this.state.transactions.filter(transaction=>transaction.id !== TransactionId)
+      this.setState({transactions: revisedTransactionList})
+    })
+  }
+
   render() {
     let searchedTransaction=this.state.transactions.filter(transaction=>transaction.description.toLowerCase().includes(this.state.search.toLowerCase()))
     return (
       <div>
         <Search search={this.state.search} handleSearch={this.handleSearch}/>
         <AddTransactionForm handleNewTransaction={this.handleNewTransaction}/>
-        <TransactionsList transactions={searchedTransaction} deleteTransacttion={this.deleteTransacttion} />
+        <TransactionsList transactions={searchedTransaction} deleteTransaction={this.deleteTransaction} />
       </div>
     );
   }
