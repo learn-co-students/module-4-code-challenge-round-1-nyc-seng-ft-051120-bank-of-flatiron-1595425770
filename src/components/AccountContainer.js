5715d67452b3daf15b2ab307ad46b0a72b8ad123
grantkoji <grantkoji@gmail.com>
2020-07-22 10:03:50 -0400

    firist deliverable

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -2,14 +2,30 @@ import React, { Component } from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
-
+const transactionsUrl = 'http://localhost:6001/transactions'
 class AccountContainer extends Component {
+  state={
+    transactions: []
+  }
+
+  componentDidMount(){
+    this.fetchTransactions()
+  }
+
+  fetchTransactions = () => {
+    fetch(transactionsUrl)
+    .then(res=>res.json())
+    .then(transactions => this.setState({transactions}))
+  }
+
+
+
   render() {
     return (
       <div>
         <Search />
         <AddTransactionForm />
-        <TransactionsList />
+        <TransactionsList transactions={this.state.transactions}/>
       </div>
     );
   }
