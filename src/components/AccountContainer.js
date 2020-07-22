cab8e0faeb4b0b52af540ad63d32b79be1c6f7b0
steveneross94 <ross.steven@me.com>
2020-07-22 10:03:30 -0400

    deliv 1

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -3,16 +3,44 @@ import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
+const BASE_URL = 'http://localhost:6001'
+
 class AccountContainer extends Component {
+
+  state = {
+    transactions: []
+  }
+
+  componentDidMount(){
+    this.getTransactions()
+  }
+
+  getTransactions = () => {
+    fetch(BASE_URL+'/transactions')
+    .then(r => r.json())
+    .then(data => {
+      this.setState({
+        transactions: data
+      })
+    })
+  }
+
+
   render() {
+    console.log(this.state);
     return (
       <div>
         <Search />
         <AddTransactionForm />
-        <TransactionsList />
+        <TransactionsList transactions={this.state.transactions}/>
       </div>
     );
   }
 }
 
 export default AccountContainer;
+
+
+// See a table of the transactions.
+// Fill out and submit the form to add a new transaction PERSIST TO BACKEND. 
+// Filter transactions by typing into the search bar
