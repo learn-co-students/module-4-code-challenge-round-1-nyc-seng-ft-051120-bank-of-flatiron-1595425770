4b1e9b1f9d4fda4ce018708152e3ae26512480a4
https://learn.co/christa-gammage <crgammage@gmail.com>
2020-07-22 10:06:05 -0400

    first deliverable

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -4,12 +4,25 @@ import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 class AccountContainer extends Component {
-  render() {
+  state = {
+    transactions: []
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:6001/transactions')
+    .then(res => res.json())
+    .then(data => {
+      this.setState({transactions: data})
+    })
+  }
+
+    render() {
+      console.log(this.state)
     return (
       <div>
         <Search />
         <AddTransactionForm />
-        <TransactionsList />
+        <TransactionsList transactions={this.state.transactions}/>
       </div>
     );
   }
