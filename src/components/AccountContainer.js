be8c0626ffe66c8ad241bd4cf9e46659e2c7dd00
smatalha <sma.talha@gmail.com>
2020-07-22 11:07:56 -0400

    2 delivarbles done

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -4,12 +4,67 @@ import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 class AccountContainer extends Component {
+  state = {
+    transactions: [],
+    search: '',
+    newTransaction: {
+      date: '',
+      description: '',
+      category: '',
+      amount: 0
+    }
+  }
+
+  componentDidMount () {
+    this.getAllTransactions()
+  }
+  getAllTransactions = () => {
+    fetch('http://localhost:6001/transactions')
+    .then(res => res.json())
+    .then(transactions => {
+      this.setState({ transactions: transactions })
+    })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    fetch('http://localhost:6001/transactions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify(this.state.newTransaction)
+    })
+    .then(res => res.json())
+    .then(newTransaction => {
+      this.setState({transactions: [...this.state.transactions, newTransaction],
+      newTransaction: {
+        date: '',
+        description: '',
+        category: '',
+        amount: 0
+      }
+      })
+    })
+  }
+  handleChange = e => {
+    this.setState ({
+      newTransaction: { ...this.state.newTransaction, [e.target.name]: e.target.value}
+    })
+  }
+
+  renderSearch = () => {
+    let filterTransaction = this.state.transactions.filter(transaction => 
+      transaction.description.toLowerCase().includes(this.state.search.toLowerCase()))
+  }
   render() {
+    // console.log(this.state);
     return (
       <div>
         <Search />
-        <AddTransactionForm />
-        <TransactionsList />
+        <AddTransactionForm handleSubmit={this.handleSubmit} handleChange={this.handleChange} newTransaction={this.state.newTransaction}/>
+        <TransactionsList transactions={this.state.transactions}/>
       </div>
     );
   }
--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -4,15 +4,17 @@ class AddTransactionForm extends Component {
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.props.handleSubmit} className="ui form">
           <div className="inline fields">
-            <input type="date" name="date" />
-            <input type="text" name="description" placeholder="Description" />
-            <input type="text" name="category" placeholder="Category" />
+            <input type="date" name="date" onChange={this.props.handleChange} value={this.props.newTransaction.date} />
+            <input type="text" name="description" onChange={this.props.handleChange} placeholder="Description" value={this.props.newTransaction.description} />
+            <input type="text" name="category" onChange={this.props.handleChange} placeholder="Category" value={this.props.newTransaction.category} />
             <input
               type="number"
               name="amount"
+              onChange={this.props.handleChange}
               placeholder="Amount"
+              value={this.props.newTransaction.amount}
               step="0.01"
             />
           </div>
