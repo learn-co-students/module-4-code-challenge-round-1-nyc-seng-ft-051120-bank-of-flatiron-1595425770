41bb84747a966ad9d98ee6e6053b49979de9549e
ChayaGreisman <chayamgreisman@gmail.com>
2020-07-22 10:24:01 -0400

    all core deliverables

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -4,12 +4,33 @@ import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 class AccountContainer extends Component {
+
+  state = {
+    transactions: [],
+    search: ""
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:6001/transactions')
+    .then(resp=>resp.json())
+    .then(transactions=> this.setState({transactions}))
+  }
+
+  handleNewTransaction = (newTransaction) => {
+    this.setState({transactions: [...this.state.transactions, newTransaction]})
+  }
+
+  handleSearchChange = e => {
+    this.setState({search: e.target.value})
+  }
+
   render() {
+    let filteredTransactions= this.state.transactions.filter(transaction=> transaction.description.toLowerCase().includes(this.state.search.toLowerCase()))
     return (
       <div>
-        <Search />
-        <AddTransactionForm />
-        <TransactionsList />
+        <Search search={this.state.search} handleChange={this.handleSearchChange}/>
+        <AddTransactionForm handleNewTransaction={this.handleNewTransaction}/>
+        <TransactionsList transactions={filteredTransactions}/>
       </div>
     );
   }
