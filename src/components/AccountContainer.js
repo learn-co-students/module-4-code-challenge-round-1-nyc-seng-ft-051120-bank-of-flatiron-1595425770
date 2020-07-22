5821cc7bdd7b9c72b4dbe9c683201376eed20be3
jnzhang622 <jnzhang622@gmail.com>
2020-07-22 11:03:44 -0400

    first commit

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -4,12 +4,74 @@ import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 class AccountContainer extends Component {
+
+  state = {
+    transactionsOG: [],
+    date: '',
+    description: '',
+    category: '',
+    amount: ''
+  }
+
+  componentDidMount() {
+    fetch(`http://localhost:6001/transactions`)
+    .then(resp => resp.json())
+    .then(arr => this.setState({ transactionsOG: arr}))
+  } 
+  
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  addNewTrans = newTrans => {
+    this.setState({
+      transactionsOG: [...this.state.transactionsOG, newTrans]
+    })
+  }
+  handleSubmit = (e) => {
+    e.preventDefault()
+
+    const newTrans = {
+      date: this.state.date,
+      description: this.state.description,
+      category: this.state.category,
+      amount: this.state.amount
+    }
+
+    fetch(`http://localhost:6001/transactions`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(newTrans)
+    })
+      .then(resp => resp.json())
+      .then(newTrans => {this.addNewTrans(newTrans)})
+      .then( ()=> this.setState({date: '', description: '', category: '', amount: ''}))
+  }
+
+  arrayRender = () => {
+    let arrayToReturn = this.state.transactionsOG.filter(transaction => {
+      return(
+        transaction
+      )
+    })
+  }
+
   render() {
+    // console.log(this.state.transactionsOG)
     return (
       <div>
         <Search />
-        <AddTransactionForm />
-        <TransactionsList />
+        <AddTransactionForm 
+          handleSubmit={this.handleSubmit} 
+          handleChange={this.handleChange}
+          date={this.state.date}
+          description={this.state.description}
+          category={this.state.category}
+          amount={this.state.amount}/>
+        <TransactionsList transactions = {this.state.transactionsOG}/>
       </div>
     );
   }
--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import AccountContainer from "./AccountContainer";
 import "../stylesheets/App.css";
 
 class App extends Component {
-  render() {
+render() {
     return (
       <div className="ui raised segment">
         <div className="ui segment violet inverted">
