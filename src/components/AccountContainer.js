a99205f24eaa43aaa08247d4a61d4c2d1516639f
https://learn.co/christa-gammage <crgammage@gmail.com>
2020-07-22 10:46:23 -0400

    final advanced deliverable

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
+import Transaction from "./Transaction";
 
 class AccountContainer extends Component {
   state = {
@@ -26,10 +27,24 @@ class AccountContainer extends Component {
     this.setState({search: e.target.value})
   }
 
+  deleteTransaction = transactionId => {
+    fetch(`http://localhost:6001/transactions/${transactionId}`, {
+      method: 'DELETE',
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      }
+    }).then(res => res.json())
+    .then(() => {
+      let newTransactionsArray = this.state.transactions.filter(transaction => transaction.id !== transactionId)
+      this.setState({transactions: newTransactionsArray})
+    })
+  }
+
   renderTransactions = () => {
     let sortedTrans = this.state.transactions.sort((a, b) => a.category < b.category ? -1 : 1)
     let filteredTransactions = sortedTrans.filter(transaction => transaction.description.toLowerCase().includes(this.state.search.toLowerCase()))
-    return <TransactionsList transactions={filteredTransactions} />
+    return <TransactionsList transactions={filteredTransactions} deleteTransaction={this.deleteTransaction}/>
   }
 
     render() {
--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 const Transaction = (props) => {
+  const {date, id, description, category, amount, deleteTransaction} = props
   return (
     <tr>
-      <td>{props.date}</td>
-      <td>{props.description}</td>
-      <td>{props.category}</td>
-      <td>{props.amount}</td>
+      <td>{date}</td>
+      <td>{description}</td>
+      <td>{category}</td>
+      <td>{amount}</td>
+      <td><button onClick={() => deleteTransaction(id)}>Delete</button></td>
     </tr>
   );
 };
